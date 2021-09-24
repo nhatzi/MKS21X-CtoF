7f3f6bd5acd0e474e2083898a5cba928851027fc
nhatzi <90657284+nhatzi@users.noreply.github.com>
2021-09-23 21:29:00 -0400

    Made procedures

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,11 @@
+class CtoFTester {
+  public int celsiusToFahrenheit()(int C_1, int F_1) {
+  F_1 = C_2 * 1.8 + 32;
+  return F_1;
+  }
 
+ public int fahrenheitToCelsius()(int C_2, int F_2) {
+  C_2 = ( ( F_2 - 32) * 5) / 9;
+  return C_2;
+  }
+} 
