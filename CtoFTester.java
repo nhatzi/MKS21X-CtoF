68e13cae088dffaa95b8041658fc558374f021c6
nhatzi <90657284+nhatzi@users.noreply.github.com>
2021-09-23 21:33:24 -0400

    Answered the questions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,10 +1,15 @@
+// The functions should have only one int parameter. This parameter should be what we are converting and, when inputted into the procedure, would return what we are converting into.
+// For example, in celsiusToFahrenheit, celcius is the parameter and will return fahrenheit. The opposite happens in fahrenheitToCelsius.
+
+
+
 class CtoFTester {
-  public int celsiusToFahrenheit()(int C_1, int F_1) {
+  public int celsiusToFahrenheit()(int C_1) {
   F_1 = C_2 * 1.8 + 32;
   return F_1;
   }
 
- public int fahrenheitToCelsius()(int C_2, int F_2) {
+ public int fahrenheitToCelsius()(int F_2) {
   C_2 = ( ( F_2 - 32) * 5) / 9;
   return C_2;
   }
